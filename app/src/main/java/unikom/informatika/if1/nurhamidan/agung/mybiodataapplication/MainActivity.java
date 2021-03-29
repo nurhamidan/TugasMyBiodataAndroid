5cdf3cbe90f6f5c4b108cfd9b05ce1ce12c7b91e
nurhamidan <agungnurhamidan@gmail.com>
2021-03-29 15:02:12 +0700

    Commit pertama

---
+++ app/src/main/java/unikom/informatika/if1/nurhamidan/agung/mybiodataapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package unikom.informatika.if1.nurhamidan.agung.mybiodataapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
